ddb0ba4474394af2e778b3c31d7a32facc5d7244
Spannenberger <63662226+spannenberger@users.noreply.github.com>
2020-04-25 03:49:07 +0500

    update

--- Tasks/TreesA.cpp
+++ Tasks/TreesA.cpp
@@ -1,4 +1,8 @@
+/*Реализуйте бинарное дерево поиска для целых чисел. Программа получает на вход последовательность целых чисел и строит из них дерево.
+Элементы в деревья добавляются в соответствии с результатом поиска их места.
+Если элемент уже существует в дереве, добавлять его не надо. Балансировка дерева не производится.*/
 #include <bits/stdc++.h>
+
 using namespace std;
 
 const int INF = 2e9 + 1337;
@@ -7,6 +11,7 @@ template<typename T>
 class BinarySearchTree {
     struct Node {
         T val;
+        int depth;
 
         Node* left = nullptr;
         Node* right = nullptr;
@@ -14,96 +19,30 @@ class BinarySearchTree {
 
         explicit Node(T val): val(val) {}
     }* root;
+    int max_depth = 0;
 
-    auto find(T x, Node* node) {
-        if (!node || node->val == x) {
-            return node;
-        }
-        if (node->val < x) {
-            return find(x, node->right);
-        }
-        return find(x, node->left);
-    }
-
-    auto min(Node* node) {
-        while (node && node->left) {
-            node = node->left;
-        }
-        return node;
+    void update_depth(Node* &node) {
+        node->depth = 1 + (node->parent ? node->parent->depth : 0);
+        max_depth = max(max_depth, node->depth);
     }
 
     void insert(T x, Node* &node) {
         if (!node) {
             node = new Node(x);
+            update_depth(node);
             return;
         }
         if (x < node->val) {
             insert(x, node->left);
             node->left->parent = node;
+            update_depth(node->left);
         } else if (x > node->val) {
             insert(x, node->right);
             node->right->parent = node;
+            update_depth(node->right);
         }
     }
 
-    void replace(Node* &node, Node* &new_node){
-        if (!node) return;
-        if (!node->parent) {
-            node = new_node;
-            if (node) {
-                node->parent = nullptr;
-            }
-            return;
-        }
-        if (node == node->parent->right) {
-            node->parent->right = new_node;
-        } else {
-            node->parent->left = new_node;
-        }
-        if (new_node) {
-            new_node->parent = node->parent;
-        }
-
-    };
-
-    void erase(T x, Node* &node) {
-        if (!node) return;
-
-        if (x < node->val) {
-            erase(x, node->left);
-        } else if (x > node->val) {
-            erase(x, node->right);
-        } else {
-            if (!node->right) {
-                replace(node, node->left);
-                return;
-            }
-            auto new_node = min(node->right);
-            node->val = new_node->val;
-            replace(new_node, new_node->right);
-        }
-    }
-
-    void next(T x, Node* node, Node* &nxt) {
-        if (!node) return;
-        next(x, node->left, nxt);
-        if (node->val > x && !nxt) {
-            nxt = node;
-            return;
-        }
-        next(x, node->right, nxt);
-    }
-
-    void prev(T x, Node* node, Node* &prv) {
-        if (!node)  return;
-
-        prev(x, node->right, prv);
-        if (node->val < x && !prv) {
-            prv = node;
-            return;
-        }
-        prev(x, node->left, prv);
-    }
 public:
     BinarySearchTree() {
         root = nullptr;
@@ -113,24 +52,8 @@ public:
         insert(x, root);
     }
 
-    void erase(T x) {
-        erase(x, root);
-    }
-
-    bool exists(T x) {
-        return find(x, root) != nullptr;
-    }
-
-    T next(T x) {
-        Node* nxt = nullptr;
-        next(x, root, nxt);
-        return nxt ? nxt->val : INF;
-    }
-
-    T prev(T x) {
-        Node* prv = nullptr;
-        prev(x, root, prv);
-        return prv ? prv->val : INF;
+    int height() {
+        return max_depth;
     }
 
     ~BinarySearchTree() {
@@ -140,31 +63,10 @@ public:
 
 void solve() {
     BinarySearchTree<int> bst;
-    for (string cmd; cin >> cmd;) {
-        int x;
-        cin >> x;
-        if (cmd == "insert") {
-            bst.insert(x);
-        } else if (cmd == "delete") {
-            bst.erase(x);
-        } else if (cmd == "next") {
-            int res = bst.next(x);
-            if (res == INF) {
-                cout << "none\n";
-                continue;
-            }
-            cout << res << "\n";
-        } else if (cmd == "prev") {
-            int res = bst.prev(x);
-            if (res == INF) {
-                cout << "none\n";
-                continue;
-            }
-            cout << res << "\n";
-        } else {
-            cout << (bst.exists(x) ? "true" : "false") << "\n";
-        }
+    for (int num; cin >> num && num;) {
+        bst.insert(num);
     }
+    cout << bst.height();
 }
 
 int main() {
